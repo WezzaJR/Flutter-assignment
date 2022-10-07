13f2cf4a0ed4cf3b8e31f67df452bb71676cf2ab
john safwat <john362sss@gmail.com>
2022-10-07 06:21:14 +0200

    add assignment number 3

---
+++ assignment-three/bin/assignment_three.dart
@@ -0,0 +1,5 @@
+import 'package:assignment_three/assignment_three.dart' as assignment_three;
+
+void main(List<String> arguments) {
+  print('Hello world: ${assignment_three.calculate()}!');
+}
---
+++ assignment-three/test/assignment_three_test.dart
@@ -0,0 +1,8 @@
+import 'package:assignment_three/assignment_three.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
